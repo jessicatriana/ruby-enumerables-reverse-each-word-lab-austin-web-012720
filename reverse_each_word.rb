313b926a7e86d80a50f9b74ad74b305dc0fc8446
Jessica Triana <jessica.triana.tx@gmail.com>
2020-01-01 17:11:01 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+require 'pry'
+
+def reverse_each_word(sentence1)
+  split_sentence = sentence1.split(" ")
+  split_sentence.collect {|word| word.reverse!}
+  new_sentence = split_sentence.join(" ")
+  new_sentence
+end
