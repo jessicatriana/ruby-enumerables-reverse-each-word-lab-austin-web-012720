8ed964faf8e2f2f873aa04f38119e64321b9f62e
Jessica Triana <jessica.triana.tx@gmail.com>
2020-01-01 17:00:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,9 +6,9 @@ def reverse_each_word(sentence1)
   
   index = 0 
   while index < split_sentence.length 
-  split_sentence.each do |split_sentence| 
+  split_sentence.each do split_sentence[index].reverse
     
-  sentence1
+ 
   binding.pry
   new_sentence
 end
