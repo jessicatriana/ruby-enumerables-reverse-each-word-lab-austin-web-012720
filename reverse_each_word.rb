239aa84ea730ba7f40b7e8efdd37701f4c09dfc2
Jessica Triana <jessica.triana.tx@gmail.com>
2020-01-01 17:05:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
 require 'pry'
 
 def reverse_each_word(sentence1)
-  new_sentence = ""
+
   split_sentence = sentence1.split(" ")
   
  split_sentence.collect {|word| word.reverse}
-    split_sentence.join()
+    split_sentence.join(" ")
  
-  binding.pry
-  new_sentence
+
+  split_sentence
 end
